a777c71551d88a60b996f56be47da300d771f277
Will Reppun <wreppun@outsideiq.com>
2014-04-25 10:33:27 -1000

    client side jshinted

--- public/js/wm.d3.js
+++ public/js/wm.d3.js
@@ -1,9 +1,8 @@
+/* global angular:false */
 angular.module('wm.d3', ['btford.socket-io'])
 .factory('Viz', ['$q', 'API', function($q, API) {
   var 
     records = {},
-    load = {},
-    availableRaces = {},
     setData;
 
   setData = function setDataFn( results ) {
@@ -22,15 +21,9 @@ angular.module('wm.d3', ['btford.socket-io'])
     race,
     donor;
 
-  var emptyCallback = function emptyCallbackFn() {
-    console.log("no callback has been set for this socket call");
-  };
-
   function createSocketCall(queryDescription, processParams) {
     var
-      deferred,
-      // functions
-      requestRecords;
+      deferred;
 
     if (!processParams) {
       processParams = function(params) {
@@ -79,41 +72,41 @@ angular.module('wm.d3', ['btford.socket-io'])
 }])
 .constant('COLORS', {
   blues: [
-    "1F75FE", //blue crayola
-    "89CFF0", //baby blue
-    "0000ff", //blue RGB
-    "0018A8", //blue pantone
-    "CCCCFF", //periwinkle
-    "333399", //blue CMYK
-    "B0E0E6", //powder blue
-    "545AA7"  //liberty
+    '1F75FE', //blue crayola
+    '89CFF0', //baby blue
+    '0000ff', //blue RGB
+    '0018A8', //blue pantone
+    'CCCCFF', //periwinkle
+    '333399', //blue CMYK
+    'B0E0E6', //powder blue
+    '545AA7'  //liberty
   ],
   reds: [
-    "FF0000", //red
-    "960018", //carmine
-    "DC143C", //crimson
-    "F88379", //coral pink
-    "ED2939"  //imperial red
+    'FF0000', //red
+    '960018', //carmine
+    'DC143C', //crimson
+    'F88379', //coral pink
+    'ED2939'  //imperial red
   ],
   greens: [
-    "568203", //avocado
-    "74c365", //mantis
-    "006400", //dark green
-    "90EE90", //light green
-    "228B22"  //forest green
+    '568203', //avocado
+    '74c365', //mantis
+    '006400', //dark green
+    '90EE90', //light green
+    '228B22'  //forest green
   ],
   yellows: []
 })
 .controller('VizParamsCtrl', function VizParamsCtrlFn($scope, Viz) {
   var 
-    NONE_AVAILABLE = "N/A",
+    NONE_AVAILABLE = 'N/A',
     ar = [];
 
   // Viz.load.availableRaces.request().then(function(results) {
     console.log(Viz);
   Viz.load.meta.availableRaces().then(function(results) {
     ar = results.data;
-    $scope.title = "";
+    $scope.title = '';
   });
 
   $scope.$watch('title+county+district+period', function(n, o, wscope) {
--- public/js/wm.donut.js
+++ public/js/wm.donut.js
@@ -1,3 +1,5 @@
+/* global angular:false, d3:false */
+
 angular.module('wm.donut', ['wm.d3'])
 .directive('donutBuilder', [ '$window', 'Viz', 'COLORS', function($window, Viz, COLORS) {
   var 
@@ -6,7 +8,7 @@ angular.module('wm.donut', ['wm.d3'])
     MIN_WIDTH = 500,
     STD_RADIUS = 200,
     // functions
-    colorize,
+    colorByParty,
     getAllRaceDonations,
     sort;
 
@@ -17,17 +19,15 @@ angular.module('wm.donut', ['wm.d3'])
     };
   }
 
-
-  colorize = function() {
+  colorByParty = (function() {
     var 
       used = {},
       getColor,
-      nextColor,
-      getAllRaceDonations;
+      nextAvailableColor;
 
     getColor = function(party, regNo) {
       if (!used[party] || !used[party][regNo]) {
-        return assignColor(party, regNo);
+        assignColor(party, regNo);
       }
 
       return used[party][regNo];
@@ -38,12 +38,11 @@ angular.module('wm.donut', ['wm.d3'])
         used[party] = {};
       }
 
-      used[party][regNo] = nextColor(party);
-      return used[party][regNo];
+      used[party][regNo] = nextAvailableColor(party);
     }
 
-    nextColor = function() {
-      partyIndex = {
+    nextAvailableColor = (function() {
+      var partyIndex = {
         democrat: 0,
         republican: 0,
         other: 0
@@ -53,12 +52,12 @@ angular.module('wm.donut', ['wm.d3'])
         var color;
 
         switch(party) {
-          case "Democrat":
+          case 'Democrat':
             color = COLORS.blues[partyIndex.democrat];
             partyIndex.democrat++;
             partyIndex.democrat = partyIndex.democrat % 7;
             break;
-          case "Republican":
+          case 'Republican':
             color = COLORS.reds[partyIndex.republican];
             partyIndex.republican++;
             partyIndex.republican = partyIndex.republican % 5;
@@ -67,16 +66,16 @@ angular.module('wm.donut', ['wm.d3'])
             color = COLORS.greens[partyIndex.other];
             partyIndex.other++;
             partyIndex.other = partyIndex.other % 5;
-            console.log("unknown party: ", party);
+            console.log('unknown party: ', party);
             break;
         }
 
         return color;
       };
-    }();
+    }());
 
     return getColor;
-  }();
+  }());
 
   getAllRaceDonations = function(d) {
     var i,
@@ -91,10 +90,10 @@ angular.module('wm.donut', ['wm.d3'])
     return donations;
   };
 
-  sort = function() {
+  sort = (function() {
     var
-      byCommittee,
-      byDonor;
+      byCommittee;
+      // byDonor;
 
     byCommittee = function byCommitteeFn(a, b) {
       // sort by committee and then by value 
@@ -108,7 +107,7 @@ angular.module('wm.donut', ['wm.d3'])
     return {
       byCommittee: byCommittee
     };
-  }();
+  }());
 
   var go = {};
 
@@ -116,9 +115,9 @@ angular.module('wm.donut', ['wm.d3'])
     isActive: false,
     go: function(index, bbox) {
       var params = {
-        title: "Mayor",
-        county: "Maui",
-        period: "2010-2012"
+        title: 'Mayor',
+        county: 'Maui',
+        period: '2010-2012'
       };
 
       Viz.load.race.allDonors(params).then(function(results) {
@@ -173,7 +172,7 @@ angular.module('wm.donut', ['wm.d3'])
 
     donutData = [];
     donutIndex = 0;
-    group = donutParent.append("g");
+    group = donutParent.append('g');
 
     pie = d3.layout.pie()
       .sort(wedgeSortFn)
@@ -211,15 +210,15 @@ angular.module('wm.donut', ['wm.d3'])
         .innerRadius(STD_RADIUS + 100*(1 - widthModifier)/(1 - GEOMETRIC_BASE) - 90*widthModifier);
 
       // returns the actionable data selection
-      arcs = group.selectAll(".arc")
+      arcs = group.selectAll('.arc')
         .data(pie(donutData));
 
       // add path DOM elements for any new data 
       arcs.enter()
-        .append("path")
-        .attr("class", "arc")
-        .attr("d", d3.svg.arc().outerRadius(10).innerRadius(1))
-        .style("fill", function(d) { 
+        .append('path')
+        .attr('class', 'arc')
+        .attr('d', d3.svg.arc().outerRadius(10).innerRadius(1))
+        .style('fill', function() { 
           return donutIndex * 111111 + '';
         })
         .on('click', function() {
@@ -228,7 +227,7 @@ angular.module('wm.donut', ['wm.d3'])
 
       // for any updated elements, redraw the arc
       arcs.transition().duration(1000)
-        .attr("d", arcGenerator);
+        .attr('d', arcGenerator);
 
       // remove dropped elements
       arcs.exit().remove();
@@ -237,10 +236,10 @@ angular.module('wm.donut', ['wm.d3'])
     };
 
     clearDonut = function clearDonutFn() {
-      console.log("pre-clear",group.selectAll(".arc"));
-      group.selectAll(".arc").data([]).exit()
+      console.log('pre-clear',group.selectAll('.arc'));
+      group.selectAll('.arc').data([]).exit()
         .transition().duration(800)
-        .attr("d", d3.svg.arc().outerRadius(0).innerRadius(0))
+        .attr('d', d3.svg.arc().outerRadius(0).innerRadius(0))
         .remove();
     };
 
@@ -250,7 +249,7 @@ angular.module('wm.donut', ['wm.d3'])
     };
   }
 
-  var donutStacker = function() {
+  var donutStacker = (function() {
     var donutStack = []; // array of active donuts
 
     /**
@@ -261,7 +260,8 @@ angular.module('wm.donut', ['wm.d3'])
     }
 
     function popStack(index) {
-      var i;
+      var i,
+        removedDonuts;
 
       removedDonuts = donutStack.splice(0, index);
 
@@ -285,7 +285,7 @@ angular.module('wm.donut', ['wm.d3'])
         updateStack();
       }
     };
-  }();
+  }());
   
   return {
     templateUrl: 'partials/donut',
@@ -298,15 +298,6 @@ angular.module('wm.donut', ['wm.d3'])
         activeZoom,
         panZoom;
 
-      vizOptions = [
-        {
-          buttonText: "",
-          isActive: true,
-          go: function() {
-
-          }
-        }
-      ];
       activeZoom = {
         translate : [0, 0],
         scale : 1
@@ -317,25 +308,28 @@ angular.module('wm.donut', ['wm.d3'])
         activeZoom.scale = d3.event.scale;
 
         bbox 
-          .attr( "transform",
-                 "translate(" + d3.event.translate + ")" +
-                 "scale(" + d3.event.scale + ")" );
+          .attr( 'transform',
+                 'translate(' + d3.event.translate + ')' +
+                 'scale(' + d3.event.scale + ')' );
       };
 
       function init() {
-        var bounds = sizeSvg(),
-          radius = Math.min(bounds.width, bounds.height) / 2 - 20;
+        var 
+          bounds,
+          zoom;
+          // radius = Math.min(bounds.width, bounds.height) / 2 - 20;
 
-        zoom = d3.behavior.zoom().on( "zoom", panZoom );
+        bounds = sizeSvg();
+        zoom = d3.behavior.zoom().on( 'zoom', panZoom );
 
-        svg = d3.select(elem[0]).append("svg:svg")
-          .attr("width", bounds.width)
-          .attr("height", bounds.height)
-          .attr("viewBox", (-bounds.width/2) + " " + (-bounds.height/2) + " " + bounds.width + " " + bounds.height)
-          .attr("preserveAspectRatio", "xMidYMid")
+        svg = d3.select(elem[0]).append('svg:svg')
+          .attr('width', bounds.width)
+          .attr('height', bounds.height)
+          .attr('viewBox', (-bounds.width/2) + ' ' + (-bounds.height/2) + ' ' + bounds.width + ' ' + bounds.height)
+          .attr('preserveAspectRatio', 'xMidYMid')
           .call( zoom );    
 
-        bbox = svg.append("g").attr("class", "bbox");
+        bbox = svg.append('g').attr('class', 'bbox');
       }
 
       // viz switching
@@ -343,16 +337,12 @@ angular.module('wm.donut', ['wm.d3'])
 
       scope.hedgers = go.hedgers;
 
-      scope.clearInnerArc = function() {
-        clearInnerArc();
-      };
-
       scope[attrs.onSubmit] = function(title, county, district, period) {
         params = {
-          title: title === "N/A" ? null : title,
-          county: county === "N/A" ? null : county,
-          district: district === "N/A" ? null : district,
-          period: period === "N/A" ? null : period
+          title: title === 'N/A' ? null : title,
+          county: county === 'N/A' ? null : county,
+          district: district === 'N/A' ? null : district,
+          period: period === 'N/A' ? null : period
         };
 
         if (scope.allDonors.isActive) {
@@ -368,9 +358,9 @@ angular.module('wm.donut', ['wm.d3'])
       init();
 
       params = {
-        title: "Mayor",
-        county: "Maui",
-        period: "2010-2012"
+        title: 'Mayor',
+        county: 'Maui',
+        period: '2010-2012'
       };
       scope.testAdd = function() {
         go.allDonors.go(0, bbox);
